97ca55adaa8bca4712552a47da6a50db2535f322
张艳华 <939593210@qq.com>
2026-03-20 16:17:17 +0800

    feat(animation): add MemorySystem lottie component

---
+++ .vitepress/theme/components/animations/lottie/MemorySystem.vue
@@ -0,0 +1,30 @@
+<template>
+  <AnimationContainer title="记忆系统演进" @restart="handleRestart">
+    <template #default="{ isVisible }">
+      <LottiePlayer
+        ref="playerRef"
+        :animation-data="animationData"
+        :autoplay="isVisible"
+        :loop="false"
+        @complete="handleComplete"
+      />
+    </template>
+  </AnimationContainer>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import AnimationContainer from '../core/AnimationContainer.vue'
+import LottiePlayer from '../core/LottiePlayer.vue'
+import animationData from './assets/memory-system.json'
+
+const playerRef = ref<InstanceType<typeof LottiePlayer> | null>(null)
+
+function handleRestart() {
+  playerRef.value?.restart()
+}
+
+function handleComplete() {
+  console.log('Memory system animation completed')
+}
+</script>
